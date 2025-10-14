53e63a0962f34714da86b8af911b0cf8b194ed00
yspireland2025 <gerf@yspi.ie>
2025-10-14 18:25:39 +0100

    Updated index.ts

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -2,7 +2,7 @@ import { createClient } from 'npm:@supabase/supabase-js@2'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Client-Info, Apikey',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
 
